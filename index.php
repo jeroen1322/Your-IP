68282fdee4a51cb7af3619228e8bad8136062484
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-06-23 13:09:54 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+     $ip=$_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+     $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+     $ip=$_SERVER['REMOTE_ADDR'];
+}
+?>
+<html>
+    <head>
+        <title>Your IP</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+        <style>
+            body{
+                padding-top:50px;
+            }
+            .starter-template{
+                padding:40px 15px;text-align:center;
+            }
+            .ip{
+                font-size: 50px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="ip"><p>Your IP address is: <strong><?php echo $ip?></strong></p></div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </body>
+</html>
